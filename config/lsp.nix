59fac458925b42545acfcd25435fc65ee1da69fb
sceredi <ceredi.simone.iti@gmail.com>
2024-02-12 22:33:42 +0100

    feat: added jdtls

--- config/lsp.nix
+++ config/lsp.nix
@@ -1,49 +1,58 @@
-{
-  plugins.lsp = {
-    enable = true;
-    servers = {
-      # c/c++
-      clangd = { enable = true; };
-      # lua
-      lua-ls = {
-        enable = true;
-        extraOptions = {
-          settings = { Lua = { telemetry = { enabled = false; }; }; };
+{pkgs, ...}: {
+  plugins = {
+    lsp = {
+      enable = true;
+      servers = {
+        # c/c++
+        clangd = { enable = true; };
+        # lua
+        lua-ls = {
+          enable = true;
+          extraOptions = {
+            settings = { Lua = { telemetry = { enabled = false; }; }; };
+          };
         };
+        # go
+        gopls.enable = true;
+        # nix
+        nixd.enable = true;
+        # python
+        pyright = { enable = true; };
+        # typescript/javascript
+        tsserver = {
+          enable = false;
+          filetypes =
+            [ "javascript" "javascriptreact" "typescript" "typescriptreact" ];
+        };
+        eslint = { enable = true; };
+        # bash
+        bashls.enable = true;
+        # css
+        cssls.enable = true;
+        # docker
+        dockerls.enable = true;
+        # html
+        html.enable = true;
+        # json
+        jsonls.enable = true;
+        # scala
+        metals.enable = true;
       };
-      # go
-      gopls.enable = true;
-      # nix
-      nixd.enable = true;
-      # python
-      pyright = { enable = true; };
-      # typescript/javascript
-      tsserver = {
-        enable = false;
-        filetypes =
-          [ "javascript" "javascriptreact" "typescript" "typescriptreact" ];
-      };
-      eslint = { enable = true; };
-      # bash
-      bashls.enable = true;
-      # css
-      cssls.enable = true;
-      # docker
-      dockerls.enable = true;
-      # html
-      html.enable = true;
-      # json
-      jsonls.enable = true;
-      # scala
-      metals.enable = true;
-    };
-    keymaps.lspBuf = {
-      "gD" = "declaration";
-      "gt" = "type_definition";
-      "K" = "hover";
+      keymaps.lspBuf = {
+        "gD" = "declaration";
+        "gt" = "type_definition";
+        "K" = "hover";
 
-      "<leader>rn" = "rename";
-      "<leader>ca" = "code_action";
+        "<leader>rn" = "rename";
+        "<leader>ca" = "code_action";
+      };
+    };
+    nvim-jdtls = {
+      enable = true;
+      data = "~/.cache/jdtls/workspace";
+      cmd = [
+        "${pkgs.jdt-language-server}/bin/jdt-language-server"
+      ];
     };
   };
 }
